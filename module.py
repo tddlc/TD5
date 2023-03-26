39e34c69bfc8c9747eb7351dd3b17feff7cb495b
tddlc <115638281+tddlc@users.noreply.github.com>
2023-03-26 23:13:00 +0200

    Update module.py

--- module.py
+++ module.py
@@ -1,18 +1,23 @@
 from requests import get
+import pandas as pd
 
 def get_manor(place_ids):
     response = get(f'https://opendomesday.org/api/1.0/place/{place_ids}')
     json_data = response.json()
     return json_data.get('manors', [])
 
+def main():
+    place_ids = place_id()
+    print(place_ids)
+    all_manor_ids = get_all_manor_ids_in_derbyshire(place_ids)
+    manors_data = get_manor(all_manor_ids)
+    manors_df = pd.DataFrame(manors_data)
+    print(manors_df)
+    
+    total_geld = manors_df['geld'].sum()
+    total_ploughs = manors_df['total_ploughs'].sum()
+    print("Total Geld Paid:", total_geld)
+    print("Total Ploughs Owned:", total_ploughs)
+
 if __name__ == '__main__':
-	place_ids=place_id()
-	print(place_ids)
-	manor_ids = get_all_manor_ids_in_derbyshire(place_ids)
-	manor_info = get_manor_info(manor_ids)
-	df = pd.DataFrame(manor_info)
-	print(df)	
-	total_geld_paid = df['geld'].sum()
-	total_ploughs_owned = df['total_ploughs'].sum()
-	print("total_geld_paid = ", total_geld_paid)
-	print("total_ploughs_owned = ", total_ploughs_owned )
+    main()
